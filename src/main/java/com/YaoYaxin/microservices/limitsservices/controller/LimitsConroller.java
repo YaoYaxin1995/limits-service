332d9f7a7366eed93e393ac24d2548f98a95b6e6
YaoYaxin1995 <yjerxiao@163.com>
2022-08-12 16:20:09 +0800

    yaoyaxin

---
+++ src/main/java/com/YaoYaxin/microservices/limitsservices/controller/LimitsConroller.java
@@ -0,0 +1,24 @@
+package com.YaoYaxin.microservices.limitsservices.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.YaoYaxin.microservices.limitsservices.bean.Limits;
+import com.YaoYaxin.microservices.limitsservices.configuration.Configuration;
+
+@RestController
+public class LimitsConroller {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+
+		return new Limits(configuration.getMinimum(),
+				configuration.getMaximum());
+		//		return new Limits(1, 1000);
+	}
+
+}
